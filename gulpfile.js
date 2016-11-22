721b07b3d475e433d207ee05965141302f15ea64
Piotr Soluch <psoluch@wiredot.com>
2016-11-22 21:11:41 +0100

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	maps = require('gulp-sourcemaps'),
+	concat = require('gulp-concat'),
+	plumber = require('gulp-plumber'),
+	notify = require('gulp-notify'),
+	minify = require('gulp-minify');
+
+gulp.task('js', function() {
+	return gulp.src('src/js/*.js')
+		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    	.pipe(minify({
+	        ext:{
+	            min:'.min.js'
+	        },
+	        noSource:true
+	    }))
+	    .pipe(gulp.dest('dist'))
+		.pipe(notify({
+			message: 'all done',
+			title: 'JS'
+		}))
+});
+
+gulp.task('themes', function() {
+	return gulp.src( 'src/themes/**/*.scss')
+		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+		.pipe(sass())
+		.pipe(gulp.dest( 'dist/themes/'))
+		.pipe(notify({
+			message: 'all done',
+			title: 'SCSS'
+		}));
+});
+
+gulp.task('default', ['themes', 'js']);
+
+gulp.task('watch', function() {
+	gulp.watch( 'src/js/*.js', ['js']);
+	gulp.watch( 'src/themes/**/*.scss', ['themes']);
+});
